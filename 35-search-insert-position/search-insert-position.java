613cc70a1d16b66fd609f8d43fa91f2d4cebf1a6
Jai Ganesh <jaiganesh15112007@gmail.com>
2026-08-08 22:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.84%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,15 +3,15 @@ class Solution {
         int l=0;
         int r=nums.length-1;
         while(l<=r){
-            int m=l+(r-l)/2;
-            if(nums[m]==target){
-                return m;
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            else if(nums[m]<target){
-                l=m+1;
+            else if(nums[mid]<target){
+                l=mid+1;
             }
             else{
-                r=m-1;
+                r=mid-1;
             }
         }
         return l;
